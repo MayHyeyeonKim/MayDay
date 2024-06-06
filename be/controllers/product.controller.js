0d8b6ff0c913d017159d66d5105e8d1b71e40f6b
MayHyeyeonKim <hkim7963@gmail.com>
2024-06-05 23:59:18 -0700

    feat: Paginate

--- be/controllers/product.controller.js
+++ be/controllers/product.controller.js
@@ -1,5 +1,6 @@
 const Product = require("../models/Product");
 
+const PAGE_SIZE = 1
 const productController = {}
 
 productController.createProduct=async(req,res) => {
@@ -15,8 +16,23 @@ productController.createProduct=async(req,res) => {
 
 productController.getProducts=async(req,res)=>{
     try{
-        const products = await Product.find({})
-        res.status(200).json({status:"success", data: products});
+        const {page, name} = req.query
+        const cond = name?{name:{$regex:name, $options:'i'}}:{};
+        let query = Product.find(cond);
+        let response = { status: "success" };
+        if (page && page > 0) {
+            query.skip((page - 1) * PAGE_SIZE).limit(PAGE_SIZE); //mongoose function
+            //최종 몇개 페이지
+            // 데이터가 총 몇개 있는지
+            // const totalItemNum = await product.find(cond).count();
+            const totalItemNum = await Product.find(cond).count();
+            // 데이터 총 개수 /PAGE_SIZE
+            const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+            response.totalPageNum = totalPageNum
+        }
+        const productList = await query.exec()
+        response.data = productList
+        res.status(200).json(response);
     }catch(error){
         res.status(400).json({status:"fail", error:error.message})
     }
